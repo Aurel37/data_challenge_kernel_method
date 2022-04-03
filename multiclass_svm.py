77574919e3c4c59ae26b503a845d4c5c2b6128d0
Sébastien Morel <sebastien.morel@eleves.enpc.fr>
2022-04-03 16:12:44 +0200

    Start controlling print messages with a verbose level

--- multiclass_svm.py
+++ multiclass_svm.py
@@ -7,7 +7,7 @@ class MultiKernelSVC:
     """Multi Class kernel SVC
     """
 
-    def __init__(self, C, dataloader, class_num, one_to_one=False, epsilon = 1e-4):
+    def __init__(self, C, dataloader, class_num, one_to_one=False, epsilon = 1e-4, verbose = -1):
         self.C = C 
         # kernel should be a function here                          
         self.kernel = dataloader.kernel    
@@ -20,6 +20,7 @@ class MultiKernelSVC:
         self.dataloader = dataloader
         self.class_num = class_num
         self.one_to_one = one_to_one
+        self.verbose = verbose
 
     def fit(self, accuracy_print=False):
         """train the multiclass svms using one vs all
@@ -27,7 +28,8 @@ class MultiKernelSVC:
         if not self.one_to_one:
             # one vs all
             for cl in range(self.class_num):
-                print(f"\r cl = {cl}", end="")
+                if verbose > 0:
+                    print(f"\r cl = {cl}", end="")
                 svc = KernelSVC(self.C, self.kernel, self.epsilon)
                 target = self.dataloader.target_train.copy()
                 target[self.dataloader.target_train == cl] = 1
@@ -36,16 +38,16 @@ class MultiKernelSVC:
                 self.SVMs.append(svc)
         else:
             # one vs one
-            # current_cl = 1
-            # class_available = np.arange(current_cl, self.class_num)
+
             size = size = self.class_num*(self.class_num - 1)/2
             current_index = 1
-            print("Begin Fit SVM oVo")
+            if verbose > 1:
+                print("Begin Fit SVM oVo")
             for class_i  in range(self.class_num):
-                
                 for class_j in range(class_i + 1, self.class_num):
                     #print('#' * int((current_index)/size * 50))
-                    print('\rProgress [{0:<50s}] current classes : ({1}, {2})'.format('#' * int((current_index)/size * 50), class_i+1, class_j+1), end="")
+                    if verbose > 0:
+                        print('\rProgress [{0:<50s}] current classes : ({1}, {2})'.format('#' * int((current_index)/size * 50), class_i+1, class_j+1), end="")
                     keep_idx = (self.dataloader.target_train == class_j) | (self.dataloader.target_train == class_i)
                     target = self.dataloader.target_train[keep_idx]
                     binary_target = np.ones(target.shape)
@@ -55,10 +57,11 @@ class MultiKernelSVC:
                     kernel_ij = self.K[keep_idx,:][:,keep_idx]
                     svc = KernelSVC(self.C, self.kernel, self.epsilon)
                     svc.fit(train_set, binary_target, kernel_ij)
-                    accuracy = svc.accuracy(train_set, binary_target)
-                    print(f" SVM ({class_i}, {class_j}) accuracy training : {accuracy}")
+                    if verbose > 1:
+                        accuracy = svc.accuracy(train_set, binary_target)
+                        print(f" SVM ({class_i}, {class_j}) accuracy training : {accuracy}")
                     self.SVMs.append(svc)
-
+                    
                     print('\rProgress [{0:<50s}] current class : {1}. {2}'.format('#' * int((current_index)/size * 50), class_i+1, acc), end="")
                     current_index += 1
 
